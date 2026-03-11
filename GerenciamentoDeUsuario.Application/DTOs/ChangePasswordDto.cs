636f8f3c8ffbf51c9f8a9c8107f9ec37e37d8884
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-11 15:15:49 -0300

    feat(application): adiciona DTOs para operações de usuario

---
+++ GerenciamentoDeUsuario.Application/DTOs/ChangePasswordDto.cs
@@ -0,0 +1,7 @@
+﻿namespace GerenciamentoDeUsuario.Application.DTOs;
+
+public class ChangePasswordDto
+{
+    public Guid UserId { get; set; } 
+    public string NovaSenha { get; set; } = string.Empty;
+}
---
+++ GerenciamentoDeUsuario.Application/DTOs/UserResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GerenciamentoDeUsuario.Application.DTOs;
+
+public class UserResponseDto
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
