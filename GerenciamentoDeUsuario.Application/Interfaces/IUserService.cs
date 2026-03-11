d04bd022d1a3d088d6f168a2eba979e9466b57d6
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-11 15:16:35 -0300

    Feat(application): Adiciona interface IUserService com casos de uso do usuário

---
+++ GerenciamentoDeUsuario.Application/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using GerenciamentoDeUsuario.Application.DTOs;
+
+namespace GerenciamentoDeUsuario.Application.Interfaces;
+
+public interface IUserService
+{
+    Task RegisterAsync(RegisterUserDto dto);
+    Task UpdateUserAsync(UpdateUserDto dto);
+    Task ChangePasswordAsync(ChangePasswordDto dto);
+    Task DeactivateAsync(Guid userId);
+    Task<UserResponseDto> GetByIdAsync(Guid id);
+}
