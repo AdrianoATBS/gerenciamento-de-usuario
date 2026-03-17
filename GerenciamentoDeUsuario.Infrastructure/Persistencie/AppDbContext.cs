77a21ab62282349962191db00ac3e15784ddfb7d
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-17 15:42:56 -0300

    feat: adiciona o DbSet RefreshToken  dentro de AppDbContext

--- GerenciamentoDeUsuario.Infrastructure/Persistencie/AppDbContext.cs
+++ GerenciamentoDeUsuario.Infrastructure/Persistencie/AppDbContext.cs
@@ -9,5 +9,16 @@ public class AppDbContext : DbContext
     {
     }
     public DbSet<User> Users { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(AppDbContext).Assembly
+         );
+    }
 
 }
