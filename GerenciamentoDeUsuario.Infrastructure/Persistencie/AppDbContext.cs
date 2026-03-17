5d7d5b99d79581a7f89bc6a23e10450e8126f946
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-17 16:25:37 -0300

    feat(infrastructure): implementa UnitOfWork em appDbContext

--- GerenciamentoDeUsuario.Infrastructure/Persistencie/AppDbContext.cs
+++ GerenciamentoDeUsuario.Infrastructure/Persistencie/AppDbContext.cs
@@ -1,16 +1,22 @@
-﻿using GerenciamentoDeUsuario.Domain.Entities;
+﻿using GerenciamentoDeUsuario.Application.Interfaces;
+using GerenciamentoDeUsuario.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace GerenciamentoDeUsuario.Infrastructure.Persistencie;
 
-public class AppDbContext : DbContext
+public class AppDbContext : DbContext, IUnitOfWork
 {
+  
    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
     public DbSet<User> Users { get; set; }
     public DbSet<RefreshToken> RefreshTokens { get; set; }
 
+    public async Task<int> SaveChangeAsync(CancellationToken cancellationTolen = default)
+    {
+        return await base.SaveChangesAsync(cancellationTolen);
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
