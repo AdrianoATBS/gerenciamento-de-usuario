3e9f8019136d89de818f17f601aa2cb54e62f280
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-17 16:08:08 -0300

    feat: implement DependencyInjection

--- GerenciamentoDeUsuario.Infrastructure/DependencyInjection/DependencyInjection.cs
+++ GerenciamentoDeUsuario.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -1,5 +1,29 @@
-﻿namespace GerenciamentoDeUsuario.Infrastructure.DependencyInjection;
+﻿using GerenciamentoDeUsuario.Domain.Interfaces;
+using GerenciamentoDeUsuario.Infrastructure.Persistencie;
+using GerenciamentoDeUsuario.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-public class DependencyInjection
+namespace GerenciamentoDeUsuario.Infrastructure.DependencyInjection;
+
+public static class DependencyInjection
 {
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<AppDbContext>(options =>
+        options.UseMySql(
+            connectionString,
+            ServerVersion.AutoDetect(connectionString)
+            )
+        );
+
+        services.AddScoped<IUserRepository,
+            UserRepository>();
+
+        return services;
+    }
 }
