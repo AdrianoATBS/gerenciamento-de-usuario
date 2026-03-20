a83ac2bdd9cecc0416ea45c3ba981076a73a597f
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-20 16:57:45 -0300

    refactor(DependencyInjection): Resolve incompatibilidade de versão do MySQL usando o AutoDetect

--- GerenciamentoDeUsuario.Infrastructure/DependencyInjection/DependencyInjection.cs
+++ GerenciamentoDeUsuario.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -18,7 +18,7 @@ public static class DependencyInjection
         services.AddDbContext<AppDbContext>(options =>
         options.UseMySql(
             connectionString,
-            new MySqlServerVersion(new Version(8,0,42))
+            ServerVersion.AutoDetect(connectionString)
             )
         );
 
