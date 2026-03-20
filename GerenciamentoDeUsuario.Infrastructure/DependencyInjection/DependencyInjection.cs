dd62a987576bec1d2ab2b1c6f71ffd404e80bc07
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-20 14:58:03 -0300

    fix:(DependencyInjection): Corrije a assinatura do método adicionando a vírgula que falta

--- GerenciamentoDeUsuario.Infrastructure/DependencyInjection/DependencyInjection.cs
+++ GerenciamentoDeUsuario.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -11,7 +11,7 @@ namespace GerenciamentoDeUsuario.Infrastructure.DependencyInjection;
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services
-        IConfiguration configuration)
+        ,IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("DefaultConnection");
 
