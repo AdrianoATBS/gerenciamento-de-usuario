1ad321a42753d1e9532ca29ad0e38035580f46c3
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-24 19:06:31 -0300

    feat: Implementa serviço de token JWT

---
+++ GerenciamentoDeUsuario.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using GerenciamentoDeUsuario.Domain.Entities;
+
+namespace GerenciamentoDeUsuario.Application.Interfaces;
+
+public interface ITokenService
+{
+    string GerarToken(User user);
+
+}
---
+++ GerenciamentoDeUsuario.Infrastructure/Services/TokenService.cs
@@ -0,0 +1,34 @@
+﻿using GerenciamentoDeUsuario.Application.Interfaces;
+using GerenciamentoDeUsuario.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace GerenciamentoDeUsuario.Infrastructure.Services;
+
+public class TokenService : ITokenService
+{
+    private readonly string _secretKey = "MINHA_SENHA_SUPER_SECRETA_123456";
+
+    public string GerarToken(User user)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Email, user.Email.Endereco),
+            new Claim(ClaimTypes.Role, user.Perfil.ToString())
+        };
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(2),
+            signingCredentials: creds
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
